b062efe444fff779600d210b8f9b0a3ee231c5d4
milovidov <milovidovgeorgy@yandex.ru>
2024-07-10 20:32:43 +0300

    fix: docker-compose.yml

--- ewm_service/src/main/java/ru/practicum/controllers/admin_api/AdminCategoriesController.java
+++ ewm_service/src/main/java/ru/practicum/controllers/admin_api/AdminCategoriesController.java
@@ -1,4 +1,35 @@
 package ru.practicum.controllers.admin_api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.category.CategoryDto;
+import ru.practicum.dto.category.NewCategoryDto;
+import ru.practicum.services.admin_api.AdminCategoriesService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/categories")
 public class AdminCategoriesController {
+    private final AdminCategoriesService adminCategoriesService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto create(@Valid @RequestBody NewCategoryDto newCategoryDto) {
+        return adminCategoriesService.create(newCategoryDto);
+    }
+
+    @DeleteMapping("{catId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long catId) {
+        adminCategoriesService.delete(catId);
+    }
+
+    @PatchMapping("{catId}")
+    public CategoryDto update(@PathVariable Long catId,
+                              @Valid @RequestBody CategoryDto categoryDto) {
+        return adminCategoriesService.update(catId, categoryDto);
+    }
 }
