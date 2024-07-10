d4bc7b5024bb78cc312d5e6e5d3374f2c747a400
milovidov <milovidovgeorgy@yandex.ru>
2024-07-10 20:54:09 +0300

    add: AdminUsersController

--- ewm_service/src/main/java/ru/practicum/controllers/admin_api/AdminUsersController.java
+++ ewm_service/src/main/java/ru/practicum/controllers/admin_api/AdminUsersController.java
@@ -1,4 +1,42 @@
 package ru.practicum.controllers.admin_api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.user.NewUserRequest;
+import ru.practicum.dto.user.UserDto;
+import ru.practicum.services.admin_api.AdminUsersService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/users")
 public class AdminUsersController {
+    private final AdminUsersService adminUsersService;
+
+    @GetMapping
+    public List<UserDto> getUsers(@RequestParam(required = false) List<Long> ids,
+                                  @RequestParam(defaultValue = "0") @Min(0) Integer from,
+                                  @RequestParam(defaultValue = "10") @Min(1) Integer size) {
+        Pageable pageable = PageRequest.of(from / size, size);
+
+        return adminUsersService.getUsers(ids, pageable);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create(@Valid @RequestBody NewUserRequest newUserRequest) {
+        return adminUsersService.create(newUserRequest);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long userId) {
+        adminUsersService.delete(userId);
+    }
 }
